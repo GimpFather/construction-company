5fa7d56f115c6bf6884869bb2208ef7ed7cafc54
GimpFather <filip.galczyk13@gmail.com>
2023-04-17 22:06:22 +0200

    OurServices section

---
+++ src/components/molecules/ServiceBlock/ServiceBlock.tsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+import UnderlinedText from "../../atoms/UnderlinedText/UnderlinedText";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+
+interface ServiceBlockProps {
+  title: string;
+  description: string;
+  icon: React.ReactElement;
+}
+
+const ServiceBlock = ({ title, description, icon }: ServiceBlockProps) => {
+  return (
+    <Stack
+      direction="column"
+      justifyContent="space-around"
+      alignItems="center"
+      spacing={2}
+      sx={{ maxWidth: "320px", marginX: 4 }}
+      component={motion.div}
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 200,
+        damping: 40,
+      }}
+    >
+      {icon}
+      <Grid item>
+        <UnderlinedText caption={title} />
+      </Grid>
+      <Grid item textAlign="center">
+        <Typography>{description}</Typography>
+      </Grid>
+    </Stack>
+  );
+};
+
+export default ServiceBlock;
--- src/components/organisms/OurServices/OurServices.tsx
+++ src/components/organisms/OurServices/OurServices.tsx
@@ -1,10 +1,34 @@
-import UnderlinedText from "@/components/atoms/UnderlinedText/UnderlinedText";
+import ServiceBlock from "@/components/molecules/ServiceBlock/ServiceBlock";
+import { Stack } from "@mui/material";
+import NoteAltIcon from "@mui/icons-material/NoteAlt";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 
 const OurServices = () => {
   return (
-    <div>
-      <UnderlinedText caption="Nasze Usługi" />
-    </div>
+    <Stack
+      direction={{ xs: "column", md: "row" }}
+      justifyContent="center"
+      alignItems={{ xs: "center", md: "flex-start" }}
+      spacing={{ xs: 6 }}
+      mt={8}
+    >
+      <ServiceBlock
+        title="Planowanie"
+        description="Nasza firma pomoże Ci zaprojektować Twój wymarzony garaż! Nasz projekt planowania garażu to kompleksowe podejście do każdego zadania, uwzględniające potrzeby i preferencje klienta."
+        icon={<NoteAltIcon color="secondary" sx={{ fontSize: "68px" }} />}
+      />
+      <ServiceBlock
+        title="Budowa"
+        description="Nasi specjaliści zawsze w stanie doradzić w kwestiach technicznych i estetycznych, a także proponować rozwiązania, które poprawią komfort i wygodę użytkowania."
+        icon={<ConstructionIcon color="secondary" sx={{ fontSize: "68px" }} />}
+      />
+      <ServiceBlock
+        title="Renowacja"
+        description="Dzięki naszemu doświadczeniu i wiedzy, jesteśmy w stanie wykonać prace remontowe i rekonstrukcyjne na najwyższym poziomie, zachowując przy tym oryginalny charakter i styl budynku."
+        icon={<RestartAltIcon color="secondary" sx={{ fontSize: "68px" }} />}
+      />
+    </Stack>
   );
 };
 
